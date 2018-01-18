aefb706c28a3269ea3e3e0beef86b6dfc57a4a74
m31271n <m31271n@users.noreply.github.com>
2018-01-18 19:23:22 +0800

    feat: add rewired logic

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 'use strict'
-module.exports = (input, opts) => {
-  if (typeof input !== 'string') {
-    throw new TypeError(`Expected a string, got ${typeof input}`)
+
+const assert = require('assert')
+const { injectBabelPlugin } = require('react-app-rewired')
+const rewireLess = require('react-app-rewire-less')
+const is = require('is')
+const isColor = require('is-color')
+
+function createRewireAntd (options) {
+  assert(
+    options && is.object(options),
+    'options should be an object.'
+  )
+
+  assert(
+    options.theme && is.string(options.theme) && isColor(options.theme),
+    'options.theme should be a valid CSS color.'
+  )
+
+  return function (config, env) {
+    config = injectBabelPlugin([
+      'import', { libraryName: 'antd', libraryDirectory: 'es', style: true }
+    ], config)
+
+    config = rewireLess.withLoaderOptions({
+      modifyVars: { '@primary-color': (options.theme || '#1DA57A') }
+    })(config, env)
+
+    return config
   }
-
-  opts = opts || {}
-
-  return input + ' & ' + (opts.postfix || 'rainbows')
 }
+
+const rewireAntd = createRewireAntd()
+
+module.exports = rewireAntd
